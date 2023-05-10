d741595eb2b2f967298c44224328133c8d040e69
hahnjihoon <95469233+hahnjihoon@users.noreply.github.com>
2023-05-10 09:30:27 +0900

    buket add222

---
+++ src/pages/Cart.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="cart">
+        카트페이지
+    </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+
+
+export default{
+    setup(){
+        const state = reactive({
+        })
+
+
+        return {state}
+    }
+}
+</script>
+
+<style scoped>
+</style>
