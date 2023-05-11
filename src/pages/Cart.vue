ad8157d7ca3ea2b947d07cfa4f88bf15cf54abd2
hahnjihoon <95469233+hahnjihoon@users.noreply.github.com>
2023-05-11 12:41:27 +0900

    buket add222

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,17 +1,33 @@
 <template>
     <div class="cart">
-        카트페이지
+        <div class="container>
+            <div>
+                <div v-for= " (i, idx) in state.items" :key="idx">
+                    <img :src="i.imgPath" />
+                    <span class="name">{{i.name}}</span>
+                    <span class="price">{{i.price}}</span>
+
+        </div>
     </div>
 </template>
 
 <script>
 import {reactive} from "vue";
+import axios from "axios"
 
 
 export default{
     setup(){
         const state = reactive({
+            items:[]
         })
+
+        axios.get("/api/cart/items").then(({data}) => {
+            console.log("data", data);
+            state.items = data;
+        })
+
+
 
 
         return {state}
